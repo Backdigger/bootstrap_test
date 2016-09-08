6ac6d62bf8a4b5ad19f144dd115be1302b61dde6
hlebedzeva <hlebedzeva@scnsoft.com>
2016-09-08 17:03:34 +0300

    img, style changes

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ jQuery(window).scroll(function(){
 
     var $sections = $('section');
     $sections.each(function(i,el){
-        var navHeight = $('.navbar-js').height();
-        var top  = $(el).offset().top-150;
+        var top  = $(el).offset().top;
         var bottom = top +$(el).height();
         var scroll = $(window).scrollTop();
         var id = $(el).attr('id');
@@ -12,17 +11,35 @@ jQuery(window).scroll(function(){
             $('a.active').removeClass('active');
             $('a[href="#'+id+'"]').addClass('active');
         }
-    })
+        var $second = $('section:eq(1)');
+        var elTop = $second.offset().top;
+
+
+        if( scroll > elTop) {
+            $('.navbar').css({"background-color": "gray", "opacity": "0.7"});
+        }
+
+        else if( scroll < $(document).height()) {
+            $('.navbar').css("background-color", "transparent");
+        }
+
+    });
+
+
+
+
 });
 
 
 $("#menu").on("click",'a', function (event) {
-    var navHeight = $('.navbar-js').height();
     event.preventDefault();
     var id  = $(this).attr('href'),
-        top = $(id).offset().top-navHeight;
+        top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 1500);
 
 });
 
+$(document).on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
 
