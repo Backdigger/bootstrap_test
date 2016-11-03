88dd49551848aa196486da80bb25ee64a0f42398
hlebedzeva <hlebedzeva@scnsoft.com>
2016-11-03 17:04:25 +0300

    Last changes

--- js/index.js
+++ js/index.js
@@ -6,13 +6,13 @@ jQuery(window).scroll(function(){
         var bottom = top +$(el).height();
         var scroll = $(window).scrollTop();
         var id = $(el).attr('id');
-        if( scroll > top && scroll < bottom){
+        if( scroll > top - 10 && scroll < bottom){
 
             $('a.active').removeClass('active');
             $('a[href="#'+id+'"]').addClass('active');
         }
         var $second = $('section:eq(1)');
-        var elTop = $second.offset().top;
+        var elTop = $second.offset().top -10 ;
 
 
         if( scroll > elTop) {
@@ -39,7 +39,44 @@ $("#menu").on("click",'a', function (event) {
 
 });
 
+$("#scroll").on("click",'a', function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+
+});
+
+
 $(document).on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+
+
+//
+// $.browser.chrome = /chrom(e|ium)/.test(navigator.userAgent.toLowerCase());
+//
+// if(!$.browser.chrome){
+//
+//
+// }
+
+var isChromium = window.chrome,
+    vendorName = window.navigator.vendor,
+    isOpera = window.navigator.userAgent.indexOf("OPR") > -1,
+    isIEedge = window.navigator.userAgent.indexOf("Edge") > -1;
+
+if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) {
+
+} else {
+    $(function() {
+        $('#menu > li').each(function(){
+            wid = $(this).width() + 20;
+            $(this).css('width', wid+'px');
+        });
+    });
+}
+
+
 
